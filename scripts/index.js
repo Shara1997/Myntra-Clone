4ed4cd853cd04c7b6b26462e67d32d83ed86638d
Shara Raj <shararaj1997@gmail.com>
2023-12-14 11:57:55 +0530

    Updated bag item count

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,43 @@
-let itemContainerElement = document.querySelector('.items-container');
+let bagItem = [];
+displayItemsOnHomePage();
+displayBagItemCount();
 
-let innerHTML = '';
-items.forEach(item => {
-  innerHTML += `
-  <div class="item-container">
-    <img class="item-img" src=${item.image} alt="item image">
-    <div class="rating">
-        ${item.rating.stars}⭐ | ${item.rating.count}k
+function addToBag(itemId) {
+  bagItem.push(itemId);
+  displayBagItemCount();
+}
+
+function displayBagItemCount() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if(bagItem.length>0){
+    bagItemCountElement.innerText = bagItem.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
+  
+}
+
+function displayItemsOnHomePage() {
+  let itemContainerElement = document.querySelector('.items-container');
+  
+  let innerHTML = '';
+  items.forEach(item => {
+    innerHTML += `
+    <div class="item-container">
+      <img class="item-img" src=${item.image} alt="item image">
+      <div class="rating">
+          ${item.rating.stars}⭐ | ${item.rating.count}k
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="pricing">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
     </div>
-    <div class="company-name">${item.company}</div>
-    <div class="item-name">${item.item_name}</div>
-    <div class="pricing">
-        <span class="current-price">Rs ${item.current_price}</span>
-        <span class="original-price">Rs ${item.original_price}</span>
-        <span class="discount">(${item.discount_percentage}% OFF)</span>
-    </div>
-    <button class="btn-add-bag">Add to Bag</button>
-  </div>
-`
-})
-itemContainerElement.innerHTML = innerHTML;
+  `
+  })
+  itemContainerElement.innerHTML = innerHTML;
+};
