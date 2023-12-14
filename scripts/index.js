9193d613b712e20135af742c0291828ba84601fb
Shara Raj <shararaj1997@gmail.com>
2023-12-14 16:35:36 +0530

    Generated html for bag summary in bag.js file and made it generic.

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,52 @@
 let bagItems;
 onLoad();
 
-
 function onLoad() {
-  let bagItemStr = localStorage.getItem('bagItems');
-  bagItems = bagItemStr ? JSON.parse(bagItemStr) : [];
+  let bagItemsStr = localStorage.getItem('bagItems');
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
   displayItemsOnHomePage();
-  displayBagItemCount();
-
+  displayBagIcon();
 }
 
 function addToBag(itemId) {
   bagItems.push(itemId);
   localStorage.setItem('bagItems', JSON.stringify(bagItems));
-  displayBagItemCount();
+  displayBagIcon();
 }
 
-function displayBagItemCount() {
+function displayBagIcon() {
   let bagItemCountElement = document.querySelector('.bag-item-count');
-  if(bagItems.length>0){
+  if (bagItems.length > 0) {
+    console.log('I am here');
     bagItemCountElement.style.visibility = 'visible';
     bagItemCountElement.innerText = bagItems.length;
   } else {
     bagItemCountElement.style.visibility = 'hidden';
   }
-  
 }
 
 function displayItemsOnHomePage() {
-  let itemContainerElement = document.querySelector('.items-container');
-  if(!itemContainerElement) {
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
     return;
   }
-  let innerHTML = '';
+  let innerHtml = '';
   items.forEach(item => {
-    innerHTML += `
+    innerHtml += `
     <div class="item-container">
-      <img class="item-img" src=${item.image} alt="item image">
+      <img class="item-image" src="${item.image}" alt="item image">
       <div class="rating">
-          ${item.rating.stars}⭐ | ${item.rating.count}k
+          ${item.rating.stars} ⭐ | ${item.rating.count}
       </div>
       <div class="company-name">${item.company}</div>
       <div class="item-name">${item.item_name}</div>
-      <div class="pricing">
+      <div class="price">
           <span class="current-price">Rs ${item.current_price}</span>
           <span class="original-price">Rs ${item.original_price}</span>
           <span class="discount">(${item.discount_percentage}% OFF)</span>
       </div>
       <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
-    </div>
-  `
-  })
-  itemContainerElement.innerHTML = innerHTML;
-};
+    </div>`
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
